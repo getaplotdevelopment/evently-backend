ea58d083492169d96a41426ff9ac796d03ee2927
Jaman Dedy <jeandedieuam@gmail.com>
2020-01-17 20:46:19 +0200

    Feature activating a user:
    - Will allow to activate a user via his email

--- helpers/sendEmail/emailTemplates.js
+++ helpers/sendEmail/emailTemplates.js
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv/config';
 
-const url = `${process.env.APP_URL}${process.env.PORT}`;
+const url =
+  process.env.NODE_ENV === 'production'
+    ? `${process.env.PRODUCTION_URL}`
+    : `${process.env.APP_URL}`;
 
 const emailTemplates = {
   resetPassword: {
@@ -9,7 +12,15 @@ const emailTemplates = {
     subject: 'Password Reset',
     html: `<h1 style="color: #444; margin-left: 20px;"> Welcome back to Evently </br> Reset your password </h1>
     <p style="color: #555; margin-left: 20px; font-size 14px"> Lost your password? click on the link below to reset it </p>
-    <a style="background-color: #61a46e; padding: 12px 15px 12px; color: #eee; font-size: 16px; text-decoration:none; margin-left:20px; cursor: pointer" href="${url}/reset-password/$token"> Reset Password </a>`
+    <a style="background-color: #61a46e; padding: 12px 15px 12px; color: #eee; font-size: 16px; text-decoration:none; margin-left:20px; cursor: pointer" href="${url}/api/reset-password/$token"> Reset Password </a>`
+  },
+  verification: {
+    from: '',
+    to: '',
+    subject: 'Email Verification',
+    html: `<h1 style="color: #444; margin-left: 20px;">Welcome to Evently</h1>
+<p style="color: #555; margin-left: 20px; font-size: 14px">Thank you for signing to Evently. Please click on the button below to activate your account.</p><br>
+<a style="background-color: #61a46e; padding: 12px 15px 12px 15px; color: #eee; font-size: 16px; text-decoration: none; margin-left: 20px; cursor: pointer;" href="${url}/api/users/verify/$token">Activate account</a>`
   }
 };
 
