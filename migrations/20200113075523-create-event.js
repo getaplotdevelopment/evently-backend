d986e31f07cb23a44782a737c3d4f6a38cf9ad91
kasulejoseph <kasule06joseph@gmail.com>
2020-03-03 22:58:52 +0300

    message

---
+++ migrations/20200113075523-create-event.js
@@ -0,0 +1,88 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      slug: {
+        type: Sequelize.STRING
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.JSON
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      tagList: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      startDate: {
+        type: Sequelize.DATE
+      },
+      finishDate: {
+        type: Sequelize.DATE
+      },
+      startTime: {
+        type: Sequelize.STRING
+      },
+      eventType: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      numberDays: {
+        type: Sequelize.STRING
+      },
+      eventImage: {
+        type: Sequelize.STRING
+      },
+      favorited: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      favoritedCount: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+      },
+      organizer: {
+        type: Sequelize.STRING,
+        model: 'Users',
+        key: 'email'
+      },
+      isDeleted: {
+        type: Sequelize.BOOLEAN
+      },
+      isLiked: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      likedBy: {
+        type: Sequelize.ARRAY(Sequelize.STRING),
+        defaultValue: []
+      },
+      currentMode: {
+        type: Sequelize.STRING,
+        defaultValue: 'draft'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Events');
+  }
+};
