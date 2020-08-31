2f6e15d299728bc5cd62980beab20f7a7514a4aa
Amani Mpangirwa JdD <jeandedieuam@gmail.com>
2020-08-31 11:35:31 +0200

    fix test (#136)
    Co-authored-by: JAMAN <macbookpro@MacBook-Pro-de-macbook.local>

--- middleware/users/canDeactivateUser.js
+++ middleware/users/canDeactivateUser.js
@@ -1,16 +1,12 @@
 import authHelper from '../../helpers/authHelper';
-import authStrategy from '../../helpers/adminOrgStrategy';
+import authStrategy from '../../helpers/authStrategy';
 import shouldDeactivateUser from '../../helpers/shouldDeactivateUser';
 
 export default async (req, res, next) => {
   const { id } = req.body;
   const email = await authHelper(req);
-  const condition = { email };
-  const condition2 = [
-    { role: 'USER' },
-    { role: 'SUPER USER' },
-    { role: 'ORGANIZER' }
-  ];
+  const condition = { email, role: 'SUPER USER' };
+  const condition2 = { email };
   const superUser = await authStrategy(condition, condition2);
   await shouldDeactivateUser(email, id);
   req.user = superUser.dataValues;
