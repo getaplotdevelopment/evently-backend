3297fcf8b92aa246413bb86242df8d893fd82ba1
JAMAN <macbookpro@MacBook-Pro-de-macbook.local>
2020-09-14 12:14:30 +0200

    fix doc

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ import swaggerUi from 'swagger-ui-express';
 import passport from 'passport';
 import http from 'http';
 import cors from 'cors';
-import createError from 'http-errors';
 import path from 'path';
 import swaggerDocument from './config/swagger.json';
-import { HTTP_NOT_FOUND } from './constants/httpStatusCode';
 import sockets from './services/socket';
 import routes from './routes';
 import 'dotenv/config';
@@ -26,5 +24,11 @@ app.use((req, _, next) => {
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
+app.use((req, res, next) => {
+  return res.status(404).json({
+    status: 404,
+    message: 'Page not found'
+  });
+});
 
 export { app, server };
