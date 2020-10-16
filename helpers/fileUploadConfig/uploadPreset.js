839ef5b9a56abd943fc404679e3f4b61802cb3d3
Kasulejoseph <kasule06joseph@gmail.com>
2020-10-16 09:56:34 +0300

    add cloudinary folder

--- helpers/fileUploadConfig/uploadPreset.js
+++ helpers/fileUploadConfig/uploadPreset.js
@@ -2,8 +2,9 @@ import { cloudinary } from '../../utils/cloudinary';
 
 const cloudinaryUploadPresetHelper = async (image, folder) => {
   const uploadedResponse = await cloudinary.uploader.upload(image, {
-    upload_preset: folder
-  });
+    upload_preset: folder,
+    folder: 'QRCodes'
+  });  
   return uploadedResponse;
 };
 
